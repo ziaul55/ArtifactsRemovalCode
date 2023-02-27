7336ca843ca14b49458b85eb3d32b486b23ed5aa
Julia Merta <56163818+julimer228@users.noreply.github.com>
2023-02-27 23:38:15 +0100

    Added benchmarks

---
+++ ArtifactsRemoval/start_processing.m
@@ -0,0 +1,6 @@
+
+loader = loader.prepare_tabels();
+loader.start_image_processing()
+loader.process_results();
+
+
--- ArtifactsRemoval/tune_new.m
+++
@@ -1,6 +0,0 @@
-% loader = tune_parameters("inputfile.json");
-% loader = loader.prepare_tabels();
-% loader.start_image_processing()
-
-loader.process_results();
-
