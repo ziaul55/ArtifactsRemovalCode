abcf80139f5f9b3f0be74e74a58166074dbd1998
julimer228 <julia-merta@wp.pl>
2023-03-28 10:41:40 +0200

    Modified gitignore
    added wsi processing
    
    removed file

---
+++ ArtifactsRemoval/load_wsi.m
@@ -0,0 +1,30 @@
+% Load WSI  
+ bim = blockedImage("..\Datasets\TCGA.svs");
+
+
+% count tresholds for each color channel (otsu method)
+layer=gather(bim,"Level",3);
+params = zeros(3,1, 'double');
+
+for i=1:3
+    [gmag, ~] = imgradient(layer(:,:,i), 'central');
+    gmag_grayscale = mat2gray(gmag);
+    params(i)=graythresh(gmag_grayscale);
+end
+
+% remove artifacts (block processing)
+sigma = 1.4;
+size_filt = 3;
+benh= apply(bim,...
+    @(bs)method2_WSI(bs.Data, params, sigma, size_filt),...
+    "BorderSize", [size_filt size_filt],"Level",1, "UseParallel",true,"OutputLocation","..\here3\","BlockSize",[1024 1024]);
+
+
+% create layers with lower resolution
+mbim = makeMultiLevel2D(benh,"Scales",[1 0.25 0.125 0.06125]);
+
+% save image
+ wadapter = images.blocked.TIFF; 
+ write(mbim, "processed.svs", "Adapter", wadapter);
+
+%img=imread(['processed.svs'],'Index',1,'PixelRegion', {[1,16000],[1,16000]});
